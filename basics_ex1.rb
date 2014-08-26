cd8c1da014031d80b7837c7daf735be666e8a341
rkstar nerd <rkstarnerd@gmail.com>
2014-08-26 19:53:13 -0400

    Intro to Programming  - The Basics Exercise 1

---
+++ basics_ex1.rb
@@ -0,0 +1,9 @@
+puts "What is your first name?"
+first_name = gets.chomp
+
+puts "What is your last name?"
+last_name = gets.chomp
+
+first_and_last = first_name + " " + last_name
+
+puts "Hi, #{first_and_last}!"
