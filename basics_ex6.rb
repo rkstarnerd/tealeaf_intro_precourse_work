cc5662d218d9cdc7b8819447962709e6a3bbce55
rkstar nerd <rkstarnerd@gmail.com>
2014-08-26 21:50:31 -0400

    Intro to Programming - The Basics Exercise 6

---
+++ basics_ex6.rb
@@ -0,0 +1,14 @@
+puts "Enter a number. Any number."
+num_one = gets.chomp.to_f
+num_one_squared = num_one*num_one
+
+puts "Enter a second number."
+num_two = gets.chomp.to_f
+num_two_squared = num_two*num_two
+
+puts "Enter a third number."
+num_three = gets.chomp.to_f
+num_three_squared = num_three*num_three
+
+puts " #{num_one} squared is #{num_one_squared}.\n #{num_two} squared is #{num_two_squared}\n #{num_three} squared is #{num_three_squared}"
+
