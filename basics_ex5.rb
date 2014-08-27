382188a6598b9376070df7c108af0d9b7d283d2d
rkstar nerd <rkstarnerd@gmail.com>
2014-08-26 21:41:38 -0400

    Intro to Programming - The Basics Exercise 5

---
+++ basics_ex5.rb
@@ -0,0 +1,9 @@
+five_factorial = 5*4*3*2*1
+six_factorial = 6*five_factorial
+seven_factorial = 7*six_factorial
+eight_factorial = 8*seven_factorial
+
+puts five_factorial
+puts six_factorial
+puts seven_factorial
+puts eight_factorial
