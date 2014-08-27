7bb0260f5c946691455126b2f4e0ab727990e8ba
rkstar nerd <rkstarnerd@gmail.com>
2014-08-26 22:18:11 -0400

    Intro to Programming - Variables Exercise 4

---
+++ cool_name.rb
@@ -0,0 +1,12 @@
+puts "What's your first name?"
+name = gets.chomp
+
+puts "Hi, #{name}!  Nice to meet you!"
+
+puts "I love your name!" 
+puts name*10
+
+puts "What's your last name?"
+last_name = gets.chomp
+
+puts "#{name} #{last_name}. Cool."
