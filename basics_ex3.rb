af4405a2f0f4d759ab001301fd7c83306b4ebd6f
rkstar nerd <rkstarnerd@gmail.com>
2014-08-26 20:31:26 -0400

    Intro to Programming - The Basics Exercise 3

---
+++ basics_ex3.rb
@@ -0,0 +1,3 @@
+movies = {"Beaches" => "1988", "The Matrix" => "1999", "The Notebook" => "2004", "Little Monsters" => "1989", "The NeverEnding Story" => "1984"}
+
+movies.each { |k, v| puts "#{v}" }
